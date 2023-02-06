ace3a5c72908199a65499f737f6afc6f402213d3
Andrey Edounov <opty@opty.ru>
2023-02-06 20:05:12 +0300

    feat: added some styled

--- src/example.tsx
+++ src/example.tsx
@@ -1,10 +1,19 @@
 import { styled } from 'linaria/react';
 
 export const Example = () => {
-  return <Root>text</Root>;
+  return (
+    <Root>
+      <Input />
+    </Root>
+  );
 };
 
 const Root = styled.div`
-  background-color: green;
+  color: #0ff;
+  background-color: red;
+  margin: 10px;
   padding: 20px;
+`;
+const Input = styled.input`
+  font-size: 17px;
 `;
