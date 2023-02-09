ce2ca2bd48175ed4adaf81159a9d53f2fead7cde
Tatiana Dorokhova <tatjana.dorokhova@yandex.ru>
2023-02-09 21:53:12 +0300

    feat: last variant

--- src/example.tsx
+++ src/example.tsx
@@ -1,9 +1,41 @@
 import { styled } from 'linaria/react';
+import React, { ChangeEvent } from 'react';
+
+const systems: Systems = {
+  celsius: {
+    formula: (v: number) => v + 30,
+    backFormula: (v: number) => v - 30
+  },
+  kelvin: { formula: (v: number) => v, backFormula: (v: number) => v }
+};
+
+type Systems = {
+  celsius: (v: number) => number;
+  kelvin: (v: number) => number;
+};
 
 export const Example = () => {
+  const [baseTemp, setBaseTemp] = React.useState(0);
+
+  const getOnChangeHandler = (key: keyof Systems) => (
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
+    const convert = systems[key];
+    const newBaseTemp = convert.backFormula(event.target.value);
+    setBaseTemp(newBaseTemp);
+  };
+
   return (
     <Root>
-      <Input />
+      {Object.entries(systems).map(([key, formulas]) => {
+        return (
+          <Input
+            key={key}
+            value={formulas.formula(baseTemp)}
+            onChange={getOnChangeHandler(key)}
+          />
+        );
+      })}
     </Root>
   );
 };
